a09d9cdc8102edc3b1760616c5face69eb2bf694
ukod <ukosname@mail.ru>
2014-12-22 19:36:36 +0200

    Начата работа над 2-ым упражнением 26го урока

---
+++ C++/HW26/2_BusStop/stopBus.cpp
@@ -0,0 +1,11 @@
+#include "stopBus.h"
+
+stopBus::stopBus()
+{
+    //ctor
+}
+
+stopBus::~stopBus()
+{
+    //dtor
+}
---
+++ C++/HW26/2_BusStop/stopBus.h
@@ -0,0 +1,14 @@
+#ifndef STOPBUS_H
+#define STOPBUS_H
+
+
+class stopBus
+{
+    public:
+        stopBus();
+        virtual ~stopBus();
+    protected:
+    private:
+};
+
+#endif // STOPBUS_H
