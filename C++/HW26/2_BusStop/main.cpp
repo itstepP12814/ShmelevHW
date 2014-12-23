ae79c41f00d0f1bc3ff23576cae41ea5737103f8
ukod <ukosname@mail.ru>
2014-12-23 21:32:28 +0300

    Работает расписание, автобусы ходят по кругу, но не в свое время, а по очереди

--- C++/HW26/2_BusStop/main.cpp
+++ C++/HW26/2_BusStop/main.cpp
@@ -1,6 +1,8 @@
 #include "ClassBus.h"
 #include "BusStop.h"
-void checkBus(ClassBus &MassOfBus);
+ClassBus spinBus(ClassBus* MassOfBus);
+
+///Автобусы приходят по расписанию на остановке, сами автобусы находятся в циклической очереди "на дороге"
 
 int main(){
     BusStop station;
@@ -12,23 +14,29 @@ int main(){
     ClassBus bus35(35,45);
     ClassBus MassOfBus[5] = {bus27,bus33,bus57,bus78,bus35};
 
+    ClassBus actualBus;
+    int timeOfActualBus;
     while(1){
         for(int i=0; i<5; ++i){
             const tm* actualTime = station.getNowTime();
-            if(MassOfBus[i].busTime == actualTime->tm_sec){
-                cout << "Bus number " << MassOfBus[i].busNumber << " on station\n";
+            ///Автобус прибывает на остановку только если его время совпадает с часами на остановке И если
+            ///время автобуса, который стоит на остановке не является временем этого же автобуса
+            if(MassOfBus[i].busTime == actualTime->tm_sec && timeOfActualBus != actualTime->tm_sec){
+                timeOfActualBus = actualTime->tm_sec;
+                cout << "Bus number " << (spinBus(MassOfBus)).busNumber << " on station\n";
             }
         }
     }
-
     return 0;
 }
 
-void spinBus(ClassBus &MassOfBus){
+///ОЧЕРЕДЬ НЕ СООТВЕТСТВУЕТ РАСПИСАНИЮ. ИСПРАВИТЬ!
+ClassBus spinBus(ClassBus* MassOfBus){
     ClassBus temp;
         temp = MassOfBus[0];
         for(int j=1; j<5; ++j){ ///Реализация циклической очереди
             MassOfBus[j-1] = MassOfBus[j];
         }
         MassOfBus[5-1] = temp;
+        return temp;
 }
