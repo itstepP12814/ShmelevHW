a9e7374012322f368b71cc3a2d0f41cf304b9736
ukod <ukosname@mail.ru>
2014-12-27 09:06:10 +0200

    мелкая правка

--- C++/lessons/lesson28/tree/main.cpp
+++ C++/lessons/lesson28/tree/main.cpp
@@ -3,13 +3,32 @@
 using namespace std;
 
 class Item{
+public:
+    Item(){};
+    ~Item(){};
     int key;
     string value;
     Item* left;
     Item* right;
     Item* next;
     Item* prev;
-};
+} root;
+
+string& operator[](int i){
+    Item** current = &root;
+    while(*current != nullptr){
+        if((*current)->key == i){
+            return *current->value;
+        }
+        if(i<(*current->key)){
+            current = &current->left;
+        }
+        else{
+            current = &current->right;
+        }
+    }
+    *current =
+}
 
 int main()
 {
