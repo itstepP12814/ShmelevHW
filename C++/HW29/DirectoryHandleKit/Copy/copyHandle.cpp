136a9893e426c0116bd797cf432f633c9d5b95ec
ukod <ukosname@mail.ru>
2015-02-05 14:41:24 +0300

    Добавлено одно исключение

--- C++/HW29/DirectoryHandleKit/Copy/copyHandle.cpp
+++ C++/HW29/DirectoryHandleKit/Copy/copyHandle.cpp
@@ -76,90 +76,100 @@ bool allFileCopy(std::string fromPath, std::string toPath, int choice){ //Метод 
 			throw std::exception("Can't create dirrectory for copying.\n");
 		}
 	}
-	int endFlag = done;
-	char buffer[256];
-	endFlag = _findnext(done, &searchResultQueueFrom);
-	endFlag = _findnext(done, &searchResultQueueFrom);
-	std::ofstream inFile;
-	std::ifstream fromFile;
+	if (done != -1){
+		int endFlag = done;
+		char buffer[256];
+		endFlag = _findnext(done, &searchResultQueueFrom);
+		endFlag = _findnext(done, &searchResultQueueFrom);
+		std::ofstream inFile;
+		std::ifstream fromFile;
 
-	while (endFlag != -1){
-		if (searchResultQueueFrom.attrib == _A_SUBDIR){ //Проверяем наличие поддиректории
-			std::string folderName = (const char*)searchResultQueueFrom.name;
-			std::string newFromPath = fromPath + "\\" + folderName;
-			std::string newToPath = toPath + "\\" + folderName;
-			_finddata_t tempData;
-			long localDone = _findfirst(newToPath.c_str(), &tempData); //Проверяем наличие аналогичной дирректории в месте назначения
-			if (localDone == -1){ //Если её нет - то создаем
-				const char* pathPtr = (newToPath.c_str());
-				int check_mkdir = _mkdir(pathPtr);
-				if (check_mkdir == -1){
-					throw std::exception("Can't create dirrectory for copying.\n");
-				}
-			}
-			else{ //Если есть - уточняем, хочет ли пользователь совместить дирректории
-				if (choice == 1 || choice == 0){ //Задаем рекурсивный повторный вопрос, если пользователь согласился на однократную замену, или если замена будет происходить впервые
-					choice = answerInterpretation(tempData);
-				}
-				if (choice == 3){ //Если пользователь хочет пропустить папку, прерываем работу функции
-					_findclose(localDone);
-					return true;
-				}
-				if (choice == 4){ //Если выбрал отмену - прерываем программу
-					_findclose(localDone);
-					throw std::exception("Operation aborted by user.\n");
-				}
-				//Если пользователь согласился на постоянную замену, перестаем задавать вопрос и пропускаем выполнение дальше
-			}
-			allFileCopy(newFromPath, newToPath, choice); //Запускаем рекурсивно функцию для копирования
-			_findclose(localDone);//Удаляем результаты поиска из памяти
-		}
-
-		else{ //Для файлов
-			std::string filename = (const char*)searchResultQueueFrom.name;
-			std::string fromPathFull = fromPath + "\\" + filename;
-			std::string toPathFull = toPath + "\\" + filename;
-			//Проверяем наличие аналогичного файла в месте назначения
-			_finddata_t fileData;
-			long filesDone = _findfirst(toPathFull.c_str(), &fileData);
-			if (filesDone != -1){ //Если файл существует
-				if (SAVE_CHOICE_STATE == false){ //Если не нужно заменять все файлы, то проверить, не появилась ли такая необходимость.
-					int answer = answerInterpretation(fileData);
-					if (answer == 2){ //Если появилась - установить флаг о постоянном пропуске
-						SAVE_CHOICE_STATE = true;
+		while (endFlag != -1){
+			if (searchResultQueueFrom.attrib == _A_SUBDIR){ //Проверяем наличие поддиректории
+				std::string folderName = (const char*)searchResultQueueFrom.name;
+				std::string newFromPath = fromPath + "\\" + folderName;
+				std::string newToPath = toPath + "\\" + folderName;
+				_finddata_t tempData;
+				long localDone = _findfirst(newToPath.c_str(), &tempData); //Проверяем наличие аналогичной дирректории в месте назначения
+				if (localDone == -1){ //Если её нет - то создаем
+					const char* pathPtr = (newToPath.c_str());
+					int check_mkdir = _mkdir(pathPtr);
+					if (check_mkdir == -1){
+						throw std::exception("Can't create dirrectory for copying.\n");
 					}
-					if (answer == 3){
-						endFlag = _findnext(done, &searchResultQueueFrom);
-						_findclose(filesDone);
-						continue;
+				}
+				else{ //Если есть - уточняем, хочет ли пользователь совместить дирректории
+					if (choice == 1 || choice == 0){ //Задаем рекурсивный повторный вопрос, если пользователь согласился на однократную замену, или если замена будет происходить впервые
+						choice = answerInterpretation(tempData);
 					}
-					if (answer == 4){
+					if (choice == 3){ //Если пользователь хочет пропустить папку, прерываем работу функции
+						_findclose(localDone);
+						return true;
+					}
+					if (choice == 4){ //Если выбрал отмену - прерываем программу
+						_findclose(localDone);
 						throw std::exception("Operation aborted by user.\n");
 					}
+					//Если пользователь согласился на постоянную замену, перестаем задавать вопрос и пропускаем выполнение дальше
 				}
+				allFileCopy(newFromPath, newToPath, choice); //Запускаем рекурсивно функцию для копирования
+				_findclose(localDone);//Удаляем результаты поиска из памяти
 			}
-			printf("Copying from %s to %s.\n", &fromPath, &toPath);
-			//std::cout << "From " << fromPathFull << " to " << toPathFull << std::endl;
-			fromFile.open(fromPathFull, std::ios::in | std::ios::binary);
-			inFile.open(toPathFull, std::ios::out | std::ios::binary);
 
-			if (!fromFile.is_open()){
-				throw std::exception("Error: access to file.\n");
-			}
-			fromFile.read(buffer, sizeof(char));
-			printf("Copying in progress...\n");
-			while (!fromFile.eof()){
-				inFile.write(buffer, sizeof(char));
+			else{ //Для файлов
+				std::string filename = (const char*)searchResultQueueFrom.name;
+				std::string fromPathFull = fromPath + "\\" + filename;
+				std::string toPathFull = toPath + "\\" + filename;
+				//Проверяем наличие аналогичного файла в месте назначения
+				_finddata_t fileData;
+				long filesDone = _findfirst(toPathFull.c_str(), &fileData);
+				if (filesDone != -1){ //Если файл существует
+					if (SAVE_CHOICE_STATE == false){ //Если не нужно заменять все файлы, то проверить, не появилась ли такая необходимость.
+						int answer = answerInterpretation(fileData);
+						if (answer == 2){ //Если появилась - установить флаг о постоянном пропуске
+							SAVE_CHOICE_STATE = true;
+						}
+						if (answer == 3){
+							endFlag = _findnext(done, &searchResultQueueFrom);
+							_findclose(filesDone);
+							continue;
+						}
+						if (answer == 4){
+							throw std::exception("Operation aborted by user.\n");
+						}
+					}
+				}
+				printf("Copying from %s to %s.\n", &fromPath, &toPath);
+				//std::cout << "From " << fromPathFull << " to " << toPathFull << std::endl;
+				fromFile.open(fromPathFull, std::ios::in | std::ios::binary);
+				inFile.open(toPathFull, std::ios::out | std::ios::binary);
+
+				if (!fromFile.is_open()){
+					throw std::exception("Error: access to file.\n");
+				}
 				fromFile.read(buffer, sizeof(char));
+				printf("Copying in progress...\n");
+				while (!fromFile.eof()){
+					inFile.write(buffer, sizeof(char));
+					fromFile.read(buffer, sizeof(char));
+				}
+				printf("\r%s -> done\n", &filename);
+				fromFile.close();
+				inFile.close();
 			}
-			printf("\r%s -> done\n", &filename);
-			fromFile.close();
-			inFile.close();
+			endFlag = _findnext(done, &searchResultQueueFrom);
 		}
-		endFlag = _findnext(done, &searchResultQueueFrom);
+		_findclose(done);
+		_findclose(doneTo);
+		return true;
 	}
-	_findclose(done);
-	_findclose(doneTo);
-	return true;
-
+	else{
+		if (doneTo == -1){
+			int dirRemoveCheck = _rmdir(toPath.c_str());
+			if (dirRemoveCheck == -1){
+				printf("Error: Can't remove a folder.\n");
+			}
+		}
+		throw std::exception("Error: Source folder does not exist.\n");
+	}
 }
