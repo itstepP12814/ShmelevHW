fad1a89a0cd9cc561e76f3bf26177187ad466558
ukod <ukosname@mail.ru>
2015-02-07 13:44:13 +0300

    Добавлена работа с map

--- C++/lessons/lesson33/Fraction_with_conversion/Fraction_with_conversion/Fration.h
+++ C++/lessons/lesson33/Fraction_with_conversion/Fraction_with_conversion/Fration.h
@@ -15,11 +15,13 @@ class Fraction
 		explicit Fraction(int a) :numerator(a), denominator(1){}; //Со словом explicit неявное преобразование запрещено
         void print()const;
         int findNOD();
-		operator double(){ return ((double)numerator) / denominator; } //Перегрузка операции преобразования Fraction в double
+		operator double()const{ return ((double)numerator) / denominator; } //Перегрузка операции преобразования Fraction в double
         const Fraction operator*(const Fraction &secondFract);
         const Fraction operator/(const Fraction &secondFract);
         const Fraction operator+(const Fraction &secondFract);
         const Fraction operator-(const Fraction &secondFract);
+
+		
 
         ///перегрузить *,/,+,-,++,--
         ///рассчитать число Пи
--- C++/lessons/lesson33/Fraction_with_conversion/Fraction_with_conversion/main.cpp
+++ C++/lessons/lesson33/Fraction_with_conversion/Fraction_with_conversion/main.cpp
@@ -1,6 +1,7 @@
 #include "Fration.h"
 #include <math.h>
-
+#include <map>
+#include <string>
 int main()
 {
 	//Fraction n = 20;
@@ -10,5 +11,20 @@ int main()
 	cout << lol;
     cout << endl;
     //n.findNOD();
+
+	map <Fraction, string> binTree;
+	binTree[Fraction(4, 8)] = "Chetyre vosmyh";
+	binTree[Fraction(5, 7)] = "Pyat Sedmyh";
+	binTree[Fraction(1, 3)] = "Odna tretia";
+
+	double needle = 0.4;
+
+	for (map<Fraction, string>::iterator itt = binTree.begin(); itt != binTree.end(); ++itt){
+		if ((double)itt->first < needle){
+			cout << itt->first << " " << itt->second;
+		}
+	}
+	
+
     return 0;
 }
