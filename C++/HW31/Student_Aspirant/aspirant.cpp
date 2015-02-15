831d44a0b2b4de5c1c44f5b6e554adc757106560
ukod <ukosname@mail.ru>
2015-02-15 18:52:45 +0300

    Добавлено наследование аспиртанта от студента

---
+++ C++/HW31/Student_Aspirant/aspirant.cpp
@@ -0,0 +1,6 @@
+#include "aspirant.h"
+
+bool Aspirant::writeWork(){
+	cout << "Aspirant write the work...\n";
+	return true;
+} 
---
+++ C++/HW31/Student_Aspirant/aspirant.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "student.h"
+class Aspirant:public Student{
+public:
+	Aspirant(string _name, int _height):Student(_name, _height){};
+	//Дочерний конструктор инициализируется базовым
+	//Инициализация базовых полей из списка инициализации
+	//дочернего констуктора невозможна.
+	~Aspirant(){};
+	bool writeWork();
+}; 
+
