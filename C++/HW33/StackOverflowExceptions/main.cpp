8658e15e98e8fcb416d37cd97570d40fc308e113
ukod <ukosname@mail.ru>
2015-02-17 08:33:35 +0200

    Добавлены упражнения 33 занятия

---
+++ C++/HW33/StackOverflowExceptions/main.cpp
@@ -0,0 +1,39 @@
+﻿/*Перепишите класс stack одного из предыдущих домашних заданий таким образом, чтобы генерировать 
+исключения для такого количества условий, какое Вы считаете приемлемым. (Например, невозможность 
+выделить нужное количество памяти, переполнение стека и т.д.)*/
+
+#include <iostream>
+#include <exception>
+
+using namespace std;
+class Stack
+{
+public:
+	Stack(){};
+	~Stack(){};
+	static int stopThisInsanity;
+	static int bytesInStack;
+	void recursiveCall(){
+		bytesInStack += sizeof(char)* 5000;
+		if (bytesInStack > 950000){
+			exception ex;
+			throw ex;
+		}
+		char trashGen[5000];
+		cout << stopThisInsanity++ << " - " << bytesInStack << " bytes in stack" << endl;
+		Stack ob;
+		ob.recursiveCall();
+	}
+};
+int Stack::stopThisInsanity = 0;
+int Stack::bytesInStack = 0;
+
+int main(){
+	try{
+		Stack ob;
+		ob.recursiveCall();
+	}
+	catch (exception ex){
+		cout << "Stack overflow.\n";
+	}
+}
