97a9c75c48dec059fc391f2261f88fe8dda4cf77
ukod <ukosname@mail.ru>
2015-03-27 22:42:57 +0300

    Добавлены полурабочие Крестики-Нолики

--- Visual C++/HW3/NoughtsAndCrosses/main.cpp
+++ Visual C++/HW3/NoughtsAndCrosses/main.cpp
@@ -1,9 +1,11 @@
 using namespace std;
 #include <Windows.h>
 #include <windowsx.h>
+#include <exception>
 #include "resource.h"
 #include <vector>
-#define CHECKING_TIMER 1
+#define CHECKING_TIMER 101
+
 class Player
 {
 public:
@@ -12,10 +14,29 @@ public:
 	~Player(){};
 } playerOne(true), playerTwo(false);
 
+struct UserPair{
+	HWND hButton;
+	char gameChar;
+};
+
+vector <UserPair> hResButtons;
+bool setGameChar(HWND hNeedle, char gameChar){
+	vector<UserPair>::iterator p = hResButtons.begin();
+	for (; (p != hResButtons.end()); ++p){
+		if (hNeedle == (*p).hButton){
+			p->gameChar = gameChar;
+			return true;
+		}
+	}
+	return false;
+}
+int buttonCounter = 0;
 int gameTurn = 0;
-vector <int> hResButtons;
+int gameOver = 0;
 BOOL CALLBACK DialogProc(HWND hWnd, UINT message, WPARAM wp, LPARAM lp);
+VOID CALLBACK TimerProc(HWND hwnd, UINT message, UINT idTimer, DWORD dwTime);
 BOOL CALLBACK EnumFunc(HWND hExWnd, LPARAM lParam);
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int nCmdShow){
 	
 	MSG msg;
@@ -30,7 +51,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpszCmdLine, 
 }
 
 BOOL CALLBACK DialogProc(HWND hWnd, UINT message, WPARAM wp, LPARAM lp){
-	//SetTimer(hWnd, CHECKING_TIMER, 1000, TimerProc);
+	if (gameOver == 0){
+		SetTimer(hWnd, CHECKING_TIMER, 50, TimerProc);
+	}
+	else{
+		KillTimer(hWnd, CHECKING_TIMER);
+	}
 	switch (message)
 	{
 		case WM_CLOSE:{
@@ -41,23 +67,32 @@ BOOL CALLBACK DialogProc(HWND hWnd, UINT message, WPARAM wp, LPARAM lp){
 		return TRUE;
 		case WM_COMMAND:
 		{
-						   
 			for (auto p : hResButtons){
-				if (LOWORD(wp) == p){
+				if ((HWND)lp == p.hButton){
 					bool parity = gameTurn % 2;
-					if (parity == playerOne.parity){
-						TCHAR gameChar[2];
-						wsprintf(gameChar, TEXT("X"));
-						SetWindowText((HWND)lp, gameChar);
-						HWND hAdviceText = GetDlgItem(hWnd, IDC_STATIC_02);
-						SetWindowText(hAdviceText, TEXT("Ходят \"O\""));
+					TCHAR gameChar[2];
+					HWND hAdviceText = GetDlgItem(hWnd, IDC_STATIC_02);
+					if (parity != playerOne.parity){
+							if (setGameChar((HWND)lp, 'X')){ //Отмечаем значение кнопки
+							wsprintf(gameChar, TEXT("X"));
+							SetWindowText((HWND)lp, gameChar);
+							SetWindowText(hAdviceText, TEXT("Ходят \"O\" (Игрок 2)"));
+						}
+						else{
+							MessageBox(hWnd, TEXT("Ошибка при выполнении хода."), TEXT("Ошибка"), MB_ICONWARNING | MB_OK);
+							return FALSE;
+						}
 					}
 					else{
-						TCHAR gameChar[2];
-						wsprintf(gameChar, TEXT("O"));
-						SetWindowText((HWND)lp, gameChar);
-						HWND hAdviceText = GetDlgItem(hWnd, IDC_STATIC_02);
-						SetWindowText(hAdviceText, TEXT("Ходят \"X\""));
+						if (setGameChar((HWND)lp, 'O')){ //Отмечаем значение кнопки
+							wsprintf(gameChar, TEXT("O"));
+							SetWindowText((HWND)lp, gameChar);
+							SetWindowText(hAdviceText, TEXT("Ходят \"X\" (Игрок 1)"));
+						}
+						else{
+							MessageBox(hWnd, TEXT("Ошибка при выполнении хода."), TEXT("Ошибка"), MB_ICONWARNING | MB_OK);
+							return FALSE;
+						}
 					}
 					gameTurn++;
 				}
@@ -73,20 +108,34 @@ BOOL CALLBACK EnumFunc(HWND hExWnd, LPARAM lParam){
 	HINSTANCE hInstance = GetModuleHandle(0);
 	TCHAR commonClassName[100];
 	GetClassName(hExWnd, commonClassName, 100);
-	int id;
 	if (!lstrcmp(commonClassName, TEXT("Button"))){
-		hResButtons.push_back(GetDlgCtrlID(hExWnd));
+		//Нумеруем кнопки
+		UserPair tempStruct = { hExWnd, '\0' };
+		hResButtons.push_back(tempStruct);
+		TCHAR text[2];
+		wsprintf(text, TEXT("%d"), hResButtons.size());
+		SetWindowText(hExWnd, text);
 	}
 	return TRUE;
 }
 
-//VOID CALLBACK TimerProc(
-//	HWND hwnd,        // handle to window for timer messages 
-//	UINT message,     // WM_TIMER message 
-//	UINT idTimer,     // timer identifier 
-//	DWORD dwTime)     // current system time 
-//{
-//	while (1){
-//		
-//	}
-//}
+VOID CALLBACK TimerProc(
+	HWND hwnd,        // handle to window for timer messages 
+	UINT message,     // WM_TIMER message 
+	UINT idTimer,     // timer identifier 
+	DWORD dwTime)     // current system time 
+{
+	if (gameOver == 0){
+		int matchCounter = 0;
+		for (auto p : hResButtons){
+			if (p.gameChar == 'X'){
+				++matchCounter;
+			}
+			if (matchCounter == 3){
+				MessageBox(hwnd, TEXT("Игра окончена"), TEXT("Game Over"), MB_OK);
+				gameOver = 1;
+				break;
+			}
+		}
+	}
+}
