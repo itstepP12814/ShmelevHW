5d1b7b8c7100951decab0a277d3e15e6a585186f
ukod <ukosname@mail.ru>
2015-06-10 17:46:25 +0300

    Мелкие правки

---
+++ C#/HW3/RevolutionArray/FreeArray.cs
@@ -0,0 +1,73 @@
+﻿/*В С # индексация начинается с нуля, но в некоторых языках программирования 
+ * это не так. Например, в Turbo Pascal индексация массиве начинается с 1.
+ * Напишите класс RangeOfArray, который  позволяет работать с массивом 
+ * такого типа, в котором индексный диапазон устанавливается пользователем.
+ * Например, в диапазоне от 6 до 10, или от -9 до 15.
+Подсказка:  В классе можно объявить две переменных, которые будут содержать
+ * верхний и нижний индекс допустимого диапазона.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevolutionArray
+{
+    class MainBlock
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                RangeOfArray<int> array = new RangeOfArray<int>(4, 8);
+
+                array[4] = 24375648;
+                array[7] = 44747474;
+                Console.WriteLine(array[4]);
+                Console.WriteLine(array[7]);
+
+                Console.ReadLine();
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Имеет место проблема размерности массива.");
+            }
+        }
+    }
+
+    class RangeOfArray <T>
+    {
+        int minBorder;
+        int maxBorder;
+        int diapason;
+        private T[] innerArray;
+
+        public RangeOfArray(int min, int max)
+        {
+            minBorder = min;
+            maxBorder = max;
+            diapason = max - min; //Реальный максимальный размер массива
+            innerArray = new T[diapason];
+        }
+
+        //Индексатор в виде шаблона, ведь мы не знаем какой тип данных будет использовать пользователь
+        public T this[int user_pos]
+        {
+            get
+            {
+                int pos = user_pos - diapason;
+                //Сдедовало бы дописать методы для сравнения других типов
+                if (pos >= innerArray.Length || pos > diapason || pos < 0)
+                    throw new IndexOutOfRangeException();
+                else
+                    return (T)innerArray[pos];
+            }
+            set
+            {
+                int pos = user_pos - diapason;
+                innerArray[pos] = (T)value;
+            }
+        }
+    }
+}
---
+++ C#/HW3/TankWar/TankWar.cs
@@ -0,0 +1,40 @@
+﻿/*Разработать программу, моделирующую танковый бой.  В танковом
+ * бою участвуют 5 танков «Т-34» и 5 танков «Pantera». Каждый 
+ * танк («Т-34» и «Pantera») описываются параметрами: «Боекомплект»,  
+ * «Уровень брони», «Уровень маневренности». Значение данных параметров 
+ * задаются случайными числами от 0 до 100. Каждый танк участвует в парной
+ * битве, т.е. первый танк «Т-34» сражается с первым танком «Pantera» 
+ * и т.д. Победа присуждается тому танку, который превышает противника
+ * по двум и более параметрам из трех (пример: см. программу).  Основное
+ * требование:  сражение (проверку на победу в бою) реализовать путем
+ * перегрузки оператора «*» (произведение).*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib;
+namespace TankWar
+{
+    class TankWar
+    {
+        static void Main(string[] args)
+        {
+            Tank tank1 = new Tank("Pantera");
+            Tank tank2 = new Tank("T-34");
+            Tank tank3 = new Tank("Pantera");
+            Tank tank4 = new Tank("T-34");
+            Tank tank5 = new Tank("Pantera");
+            Tank tank6 = new Tank("T-34");
+            Console.WriteLine(tank1.ToString());
+            Console.WriteLine(tank2.ToString());
+            Console.WriteLine(tank3.ToString());
+            Console.WriteLine(tank4.ToString());
+            Console.WriteLine(tank5.ToString());
+            Console.WriteLine(tank6.ToString());
+            Console.WriteLine("Бой первый. Победа за {0}", tank1 * tank2);
+            Console.WriteLine("Бой второй. Победа за {0}", tank3 * tank4);
+            Console.WriteLine("Бой третий. Победа за {0}", tank5 * tank6);
+        }
+    }
+}
