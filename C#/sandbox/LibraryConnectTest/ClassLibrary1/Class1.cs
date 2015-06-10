3cf9b4ac984b4b2f4b9db8ffdf80e4c3ab437070
ukod <ukosname@mail.ru>
2015-06-10 15:25:58 +0300

    Новые тесты возможностей языка

---
+++ C#/sandbox/LibraryConnectTest/ClassLibrary1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib
+{
+    public class Tank
+    {
+        string name;
+        int ammunition;
+        int armor;
+        int speed;
+
+        public Tank(string tankModel)
+        {
+            if (tankModel.ToLower() == "pantera")
+            {
+                name = "Pantera";
+            }
+            else
+            {
+                if (tankModel.ToLower() == "t-34" || tankModel.ToLower() == "t34")
+                {
+                    name = "T-34";
+                }
+            }
+            Random rand = new Random();
+            ammunition = rand.Next(0, 100);
+            armor = rand.Next(0, 100);
+            speed = rand.Next(0, 100);
+        }
+        public override string ToString()
+        {
+            return name + "\t" + ammunition.ToString() + "\t" + armor.ToString() + "\t" + speed.ToString();
+        }
+    }
+}
---
+++ C#/sandbox/LibraryConnectTest/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank loly = new Tank("Pantera");
+            Console.WriteLine(loly.ToString());
+        }
+    }
+}
