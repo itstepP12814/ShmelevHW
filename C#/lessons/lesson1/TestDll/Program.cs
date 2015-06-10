c5934c53b171ec6aba86b9d3af40a2b4f2ddefd4
ukod <ukosname@mail.ru>
2015-06-10 15:25:25 +0300

    Пример на подключение бибилиотеки

---
+++ C#/lessons/lesson1/TestDll/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MainClassLib;
+namespace TestDll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainClassLib.Car car = new MainClassLib.Car();
+            car.getN();
+        }
+    }
+}
